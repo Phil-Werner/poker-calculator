ab5133405fb28c1daef03292a15d23f75b308ebd
Phil Werner <philwerner@shaw.ca>
2019-02-01 05:31:29 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import club from './club.jpg';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function click(event, rank, suit) {
+  event.preventDefault();
+  console.log("You clicked on the",rank, "of", suit, "s");
+}
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+      </div>
+      <div className="test">
+        testtt!!!
+      </div>
+      <div className="top-card" id="ace-of-clubs" onClick={(e) => {
+        click(e, "A", "club")
+      }}>
+        A<img src={club} alt="club" id="top-club" />
+      </div>
+      <div className="top-card" id="king-of-clubs" onClick={(e) => {
+        click(e, "K", "club")
+      }}>
+        K<img src={club} alt="club" id="top-club" />
+      </div>
+      <div className="top-card" id="queen-of-clubs" onClick={(e) => {
+        click(e, "Q", "club")
+      }}>
+        Q<img src={club} alt="club" id="top-club" />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
